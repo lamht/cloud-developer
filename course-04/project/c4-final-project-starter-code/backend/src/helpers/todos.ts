09f644ff0f72717ed6b3c241a48d8630b09ff6e3
Lam Huynh <thanhlam0290@gmail.com>
2023-03-14 00:25:50 +0700

    update code

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -5,6 +5,45 @@ import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+// import * as createError from 'http-errors'
+
+const logger = createLogger('Todo-Business-Logic') 
+const todosAccess = new TodosAccess()
+const attachmentUtils = new AttachmentUtils()
 
 // TODO: Implement businessLogic
+export async function createTodo(userId: string, newTodo: CreateTodoRequest): Promise<TodoItem>{
+    logger.info(`create todo: user ${userId}, data ${newTodo}`);
+    const todoId = uuid.v4();
+    logger.info('Create TODO with generated uuid', { todoId });
+
+    const newTodoItem: TodoItem = {
+        userId,
+        todoId,
+        createdAt: new Date().toISOString(),
+        ...newTodo,
+        done: false
+    }
+
+
+    return await todosAccess.createTodo(newTodoItem);
+}
+
+export async function getTodosForUser(userId: string): Promise<TodoItem[]>{
+    return await todosAccess.getTodos(userId)
+}
+
+export async function deleteTodo(userId: string, todoId: string){
+    logger.info(`delete todo, userId ${userId}, todoId ${todoId}`);
+    return await todosAccess.deleteTodo(todoId, userId)
+}
+
+export async function createAttachmentPresignedUrl(userId: string, todoId: string): Promise<string>{
+    logger.info(`create Presigned Url, userId ${userId}, todoId ${todoId}`);
+    return await attachmentUtils.getSignedUrl(todoId);
+}
+
+export async function updateTodo(userId: string, todoId: string, updatedTodo: UpdateTodoRequest) {
+    logger.info(`update todo, userId ${userId}, todoId ${todoId}, data ${updatedTodo}`);
+    return await todosAccess.updateTodo(todoId, userId, updatedTodo)
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,25 +1,38 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDHTCCAgWgAwIBAgIJBspOoEXnqyD9MA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
+BAMTIWRldi14a2RveDd2OGppcWMzMTd1LnVzLmF1dGgwLmNvbTAeFw0yMzAzMDgw
+MTU4MTVaFw0zNjExMTQwMTU4MTVaMCwxKjAoBgNVBAMTIWRldi14a2RveDd2OGpp
+cWMzMTd1LnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBALB+QSyhNjXTEzV/PCwIP0aWIl5grQmqu21XkRIjRnrDRKM1CVMkDJ2BAtJV
+zPMLMgOHKPJQgCrJcI9eMuDkyHtBndLB3Fi9UloddcbxW96hiSk2btFtci5fc7E0
+NzcudNTmXn2eiFYTEZlY2UC+twefUEcTa+7sgszWSJilBYOvvrZ8cICjgDnsgL99
+boxHw/XIe8rn72fxzbZn+LOFLByw70dTKc3WlImvYSQ5Rk1I2bGv6/rV9rTQLnJa
+EjJhtcBwOL7TFzVqGZqigTa7kYQ11OmbmR+VtBIDTcArwLYdA0zttLvNLfl7EuMD
+Sp6UpLpC0vo8Ntbau9K7ymDaMj0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
+BgNVHQ4EFgQUAxZUTcOQQWK4WSF7pEr2js1P6cowDgYDVR0PAQH/BAQDAgKEMA0G
+CSqGSIb3DQEBCwUAA4IBAQCtLHWng9u5B/sAXlEM3Y/rwt1vyI1JsgJyU/CRrwbs
+z06j2zB8gPLVejloc0hyvWGF8mF/far2hhUhka5Mrsd/BqYD2BlF8bluWTiTddZo
+DUjDqzhv2CKsK9Dunf1gMRKLjMIZ/qcpHLKQDSgJt5csrLfUpCMkVb69DVr9hrQs
+Qyl5YzcOqQEOcMmBSagKMmU9fMqrJTer+vIHbTVHvG7BP7K8MiHeNUQ4w+YCXZhr
+aK+cHoSaWKKkvy8oMYHUoO+8VV/3/un6CH+BMA2iYR1jd4ciOnGftvSmR8B623bm
+nEW5YAn2rzblVOV1D4sRwWCOkzkh6syONiq+gqP4HwNW
+-----END CERTIFICATE-----`
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
-    const jwtToken = await verifyToken(event.authorizationToken)
+    const jwtToken = verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -54,14 +67,9 @@ export const handler = async (
   }
 }
 
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
+function verifyToken(authHeader: string): JwtPayload {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return verify(token, cert, {algorithms: ['RS256']}) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
