5f80c2f8a444fc27b0dbfe2e3b660e77861ac98b
Lam Huynh <thanhlam0290@gmail.com>
2023-03-14 12:00:23 +0700

    Update config.ts

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -7,5 +7,6 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-xkdox7v8jiqc317u.us.auth0.com',            // Auth0 domain
   clientId: 'Mxp26N8nJUBkka4h0CHen6DKlMTTjbak',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  //callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://serverless-todo-app-frontend-dev-9skd.s3-website-us-east-1.amazonaws.com/callback'
 }
